b2affb0006827fdf3abe00bcc1204e58a89280f1
Lotharan <dklotharan@hotmail.com>
2017-04-17 22:42:59 -0400

    working copy

--- app.js
+++ app.js
@@ -4,44 +4,31 @@ var shoppingList = {
 		this.listItems.push(newItem);
 		this.renderList();
 	},
+	deleteListItem: function(str) {
+		this.listItems = this.listItems.filter(function(item){
+			return (item !== str)
+		});
+		this.renderList();
+	},
 	renderList: function(){
 		$('.shopping-list').empty();
 		for (var i = 0; i < this.listItems.length; i++){
 			$('.shopping-list').append('<li>' + '<span class="shopping-item">' + this.listItems[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
 		}
 		buttonClicks();
-	},
-	deleteListItem: function(str) {
-		this.listItems = this.listItems.filter(function(item){
-			return (item === str)
-		});
-		this.renderList();
 	}
 }
 
-$('#add-item').click(function() {
+$('#js-shopping-list-form').on('submit', function(event){
+	event.preventDefault();
 	shoppingList.addListItem($('#shopping-list-entry').val());
 });
 
 function buttonClicks(){
 	$( '.shopping-item-toggle').on( 'click', function( event ) {
-		$( event.target ).toggleClass('shopping-item__checked');
-	});
+		$(event.currentTarget.closest('li')).find('.shopping-item').toggleClass('shopping-item__checked');
+		});
 	$('.shopping-item-delete').on('click', function(event){
-		shoppingList.deleteListItem($(event.target).closest('.shopping-item').text());
-		console.log('click');
-		console.log($(event.target).closest('.shopping-item').text())
+		shoppingList.deleteListItem($(event.currentTarget.closest('li')).find('.shopping-item').text());
 	});
 }
-
-// function addListItem(){
-// 	var newItem = $('#shopping-list-entry').val();
-// 	$(".shopping-list").append("<li>" + newItem + "</li>");
-// 		};
-// 	}
-
-	// .remove()
-// $( "button" ).click(function( event ) {
-// 	event.preventDefault();
-
-// push into array, show in the DOM, and when removing it take out of the DOM
