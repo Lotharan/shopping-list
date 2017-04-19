8d915821b9927975e8574262fe97657157979538
Lotharan <dklotharan@hotmail.com>
2017-04-18 21:58:47 -0400

    refactored and corrected delete item function

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ var shoppingList = {
 		this.renderList();
 	},
 	deleteListItem: function(str) {
-		this.listItems = this.listItems.filter(function(item){
-			return (item !== str)
-		});
+		// this.listItems = this.listItems.filter(function(item){
+		// 	return (item !== str)
+		// });
+		this.listItems.splice(this.listItems.indexOf(str), 1);
 		this.renderList();
 	},
 	renderList: function(){
 		$('.shopping-list').empty();
 		for (var i = 0; i < this.listItems.length; i++){
-			$('.shopping-list').append('<li>' + '<span class="shopping-item">' + this.listItems[i] + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>');
+			$('.shopping-list').append('<li>' +
+				'<span class="shopping-item">' + this.listItems[i] + '</span>' +
+					'<div class="shopping-item-controls">' +
+						'<button class="shopping-item-toggle">' +
+							'<span class="button-label">check</span>' +
+						'</button>' +
+						'<button class="shopping-item-delete">' +
+							'<span class="button-label">delete</span>' +
+						'</button>' +
+					'</div>' +
+				'</li>');
 		}
 		buttonClicks();
 	}
